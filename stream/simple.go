f18014492cf5252c3e326d6e79e36c8318e02cd6
Pascal S. de Kloe <pascal@quies.net>
2023-02-21 19:15:24 +0100

    writev(2) limit and partial write handling

--- stream/simple.go
+++ stream/simple.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"math/bits"
 	"os"
+	"runtime"
 	"syscall"
 	"unsafe"
 )
@@ -51,43 +52,83 @@ func (w simpleFDWriter) Write(batch []Entry) error {
 	if len(batch) > cap(w.headers) {
 		w.headers = make([][4]byte, len(batch))
 		w.vectors = make([]syscall.Iovec, 0, len(batch)*3)
-	} else {
-		w.headers = w.headers[:len(batch)]
-		w.vectors = w.vectors[:0]
 	}
 
+	// collect the buffers to be written
+	v := w.vectors[:0]
 	for i := range batch {
 		if len(batch[i].MediaType) > 255 || len(batch[i].Payload) > 0xFF_FFFF {
 			return ErrSizeMax
 		}
 		binary.BigEndian.PutUint32(w.headers[i][:], uint32(len(batch[i].Payload)<<8|len(batch[i].MediaType)))
-		w.vectors = append(w.vectors, syscall.Iovec{&w.headers[i][0], 4})
+		v = append(v, syscall.Iovec{&w.headers[i][0], 4})
 		if batch[i].MediaType != "" {
 			// go won't allow address of string content
 			b := (*[]byte)(unsafe.Pointer(&batch[i].MediaType))
-			w.vectors = append(w.vectors, syscall.Iovec{&(*b)[0], uint64(uint(len(batch[i].MediaType)))})
+			v = append(v, syscall.Iovec{&(*b)[0], uint64(uint(len(batch[i].MediaType)))})
 		}
 		if len(batch[i].Payload) != 0 {
-			w.vectors = append(w.vectors, syscall.Iovec{&batch[i].Payload[0], uint64(uint(len(batch[i].Payload)))})
+			v = append(v, syscall.Iovec{&batch[i].Payload[0], uint64(uint(len(batch[i].Payload)))})
 		}
 	}
 
-	// IOV_MAX from <limits.h> unavailable
-	const max = 1024
+	// write all of v
+	max := vectorLimit()
+	for len(v) != 0 {
+		l := len(v)
+		if l > max {
+			l = max
+		}
+		wrote, _, errno := syscall.Syscall(syscall.SYS_WRITEV, w.fd, uintptr(unsafe.Pointer(&v[0])), uintptr(l))
 
-	for offset := 0; offset < len(w.vectors); offset += max {
-		n := len(w.vectors) - offset
-		if n > max {
-			n = max
+		// apply write count; -1 means aborted
+		if wrote != 0 && wrote != ^uintptr(0) {
+			pass := uint64(wrote)
+			for i := range v {
+				switch l := v[i].Len; {
+				case l < pass:
+					pass -= l
+					continue
+
+				case l == pass:
+					// v[i] fully written
+					v = v[i+1:]
+				case l > pass:
+					// v[i] partial write
+					v = v[i:]
+					v[0].Len -= l
+					v[0].Base = (*byte)(unsafe.Pointer((uintptr(unsafe.Pointer(v[0].Base)) + uintptr(l))))
+				}
+				break
+			}
 		}
 
-		_, _, errno := syscall.Syscall(syscall.SYS_WRITEV, w.fd, uintptr(unsafe.Pointer(&w.vectors[offset])), uintptr(n))
-		if errno != 0 {
+		// handle error
+		switch errno {
+		case 0, syscall.EINTR:
+			break
+		case syscall.EAGAIN:
+			// BUG(pascaldekloe): Non-blocking I/O left unchecked.
+			fallthrough
+		default:
 			return errno
 		}
 	}
 
 	return nil
+}
+
+// VectorLimit returns the maximum amount of iovec(2) allowed per writev(2).
+func vectorLimit() int {
+	// IOV_MAX from <limits.h> unavailable
+	// https://github.com/golang/go/issues/58623
+	max := 1024
+	// copied from internal/poll/writev.go; watch for updates
+	if runtime.GOOS == "aix" || runtime.GOOS == "solaris" {
+		// IOV_MAX is set to XOPEN_IOV_MAX on AIX and Solaris.
+		max = 16
+	}
+	return max
 }
 
 type simpleReader struct {
