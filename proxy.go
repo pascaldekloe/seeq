b8bb9361af0642861b4d173b8971354f11f1b2aa
Pascal S. de Kloe <pascal@quies.net>
2023-07-11 12:32:57 +0200

    proxy both sequential and parallel delegates

--- proxy.go
+++ proxy.go
@@ -1,16 +1,111 @@
 package seeq
 
-import "github.com/pascaldekloe/seeq/stream"
+import (
+	"errors"
 
-// UnmarshalProxy provides an Aggregate[stream.Entry] interface for a collection
-// of delegates. Each stream.Entry does unmarshal once only.
+	"github.com/pascaldekloe/seeq/stream"
+)
+
+// AddNextCall has the arguments of an Aggregate invocation.
+type addNextCall[T any] struct {
+	batch  []T
+	offset uint64
+}
+
+// Proxy forwards Aggregate (AddNext) invocation to its delegates. Delagates
+// from the constructor get invoked sequentially. AddParallel launches its own
+// routines.
+type Proxy[T any] struct {
+	sequential []Aggregate[T]
+
+	parallelN int
+	// no buffer for low latency and more efficent handover
+	parallelFeed chan addNextCall[T]
+	parallelErrs chan error
+	parallelDone chan struct{}
+}
+
+// NewProxy is the default constructor. The arguments, if any, get invoked
+// consecutively in order of appearence for every AddNext.
+func NewProxy[T any](sequential ...Aggregate[T]) *Proxy[T] {
+	return &Proxy[T]{
+		sequential:   sequential,
+		parallelN:    0,
+		parallelFeed: make(chan addNextCall[T]),
+		parallelErrs: make(chan error),
+		parallelDone: make(chan struct{}),
+	}
+}
+
+// AddParallel launches a new goroutine dedicaded to the Aggregate. Use Halt to
+// clean up if needed. Slow aggregation may bennefit from parallelism at the
+// cost of two channel transfers per AddNext: slice plus 64 bits, and an error.
+func (p *Proxy[T]) AddParallel(agg Aggregate[T]) {
+	p.parallelN++
+
+	go func() {
+		for call := range p.parallelFeed {
+			p.parallelErrs <- agg.AddNext(call.batch, call.offset)
+		}
+		p.parallelDone <- struct{}{}
+	}()
+}
+
+// Halt removes all Aggregates, and it stops all parallel routines. Use only
+// after aggregation stopped. Most usecases will run their aggregates for the
+// entire lifespan of the application though.
+func (p *Proxy[T]) Halt() {
+	p.sequential = nil
+
+	close(p.parallelFeed)
+	for n := p.parallelN; n > 0; n-- {
+		<-p.parallelDone
+	}
+	p.parallelFeed = nil
+	p.parallelN = 0
+}
+
+// AddNext implements the Aggregate interface.
+func (p *Proxy[T]) AddNext(batch []T, offset uint64) error {
+	// any and all parallel routines should be waiting at this point
+	for n := p.parallelN; n > 0; n-- {
+		p.parallelFeed <- addNextCall[T]{batch, offset}
+	}
+
+	var errs []error
+
+	// run sequential list
+	for i := range p.sequential {
+		err := p.sequential[i].AddNext(batch, offset)
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}
+
+	// await and collect parallel results
+	for n := p.parallelN; n > 0; n-- {
+		// each routine sends one error—nil or not
+		err := <-p.parallelErrs
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}
+	// any and all parallel routines should be waiting again
+
+	if len(errs) != 0 {
+		return errors.Join(errs...)
+	}
+	return nil
+}
+
+// UnmarshalProxy provides an Aggregate[stream.Entry] interface for an
+// Aggregate[T] delegate.
 type UnmarshalProxy[T any] struct {
-	// Aggregate delegates get invoked sequentially in slice order.
-	Aggs []Aggregate[T]
+	Aggregate[T]
 
-	// UnmarshalInto sets the out pointer to the interpretation of in. The
-	// stream entry is dropped when the return is false. Implementations are
-	// encouraged to recycle (parts of) T.
+	// UnmarshalInto sets pointer out to the interpretation of in. A false
+	// return causes the stream entry to be skipped instead. Implementations
+	// are encouraged to recycle (parts of) T.
 	UnmarshalInto func(out *T, in stream.Entry, offset uint64) bool
 
 	buf []T
@@ -31,7 +126,7 @@ func (p *UnmarshalProxy[T]) AddNext(batch []stream.Entry, offset uint64) error {
 			continue
 		}
 
-		err := p.addNextEach(p.buf[since:i], offset+since)
+		err := p.Aggregate.AddNext(p.buf[since:i], offset+since)
 		if err != nil {
 			return err
 		}
@@ -39,15 +134,5 @@ func (p *UnmarshalProxy[T]) AddNext(batch []stream.Entry, offset uint64) error {
 		since = uint64(uint(i + 1)) // skip failed unmarshal
 	}
 
-	return p.addNextEach(p.buf[since:], offset+since)
-}
-
-func (p *UnmarshalProxy[T]) addNextEach(batch []T, offset uint64) error {
-	for i := range p.Aggs {
-		err := p.Aggs[i].AddNext(batch, offset)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
+	return p.Aggregate.AddNext(p.buf[since:], offset+since)
 }
--- proxy_test.go
+++ proxy_test.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"log"
 	"math/big"
+	"slices"
+	"strings"
+	"testing"
 
 	"github.com/pascaldekloe/seeq"
 	"github.com/pascaldekloe/seeq/stream"
@@ -33,31 +36,15 @@ func ExampleUnmarshalProxy() {
 	var c Counts
 
 	proxy := seeq.UnmarshalProxy[big.Int]{
-		Aggs: []seeq.Aggregate[big.Int]{&c},
-		UnmarshalInto: func(out *big.Int, in stream.Entry, offset uint64) bool {
-			t := stream.CachedMediaType(in.MediaType)
-			switch {
-			case t.Subtype == "octet-stream":
-				out.SetBytes(in.Payload)
-				return true
-
-			case t.Type == "text":
-				_, ok := out.SetString(string(in.Payload), 10)
-				if !ok {
-					log.Printf("malformed decimal at stream offset %d", offset)
-					return false
-				}
-				return true
-			}
-
-			return false
-		},
+		Aggregate:     &c,
+		UnmarshalInto: unmarshalInto,
 	}
 
-	err := proxy.AddNext([]stream.Entry{
+	samples := []stream.Entry{
 		{"application/octet-stream", []byte{1}},
 		{"text/plain", []byte{'2'}},
-	}, 9765625)
+	}
+	err := proxy.AddNext(samples, 9765625)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -65,4 +52,97 @@ func ExampleUnmarshalProxy() {
 	fmt.Printf("got %d in total; average of %s\n", &c.Sum, c.Avg().FloatString(2))
 	// Output:
 	// got 3 in total; average of 1.50
+}
+
+// UnmarshalInto sets the out pointer to the interpretation of in, confrom the
+// UnmarshalProxy structure.
+func unmarshalInto(out *big.Int, in stream.Entry, offset uint64) bool {
+	t := stream.CachedMediaType(in.MediaType)
+	switch {
+	case t.Subtype == "octet-stream":
+		out.SetBytes(in.Payload)
+		return true
+
+	case t.Type == "text":
+		_, ok := out.SetString(string(in.Payload), 10)
+		if !ok {
+			log.Printf("malformed decimal at stream offset %d", offset)
+			return false
+		}
+		return true
+	}
+
+	return false
+}
+
+type intRecord struct {
+	offsets []uint64
+	values  []int64
+}
+
+func (r *intRecord) AddNext(batch []big.Int, offset uint64) error {
+	for i := range batch {
+		r.offsets = append(r.offsets, offset+uint64(i))
+		r.values = append(r.values, batch[i].Int64())
+	}
+	return nil
+}
+
+func TestUnmarshalProxy(t *testing.T) {
+	tests := []struct {
+		feed        []string
+		wantOffsets []uint64
+		wantValues  []int64
+	}{
+		{
+			feed:        []string{},
+			wantOffsets: []uint64{},
+			wantValues:  []int64{},
+		},
+		{
+			feed:        []string{"1", "2", "7"},
+			wantOffsets: []uint64{1000, 1001, 1002},
+			wantValues:  []int64{1, 2, 7},
+		},
+		{
+			feed:        []string{"1", "b", "7"},
+			wantOffsets: []uint64{1000, 1002},
+			wantValues:  []int64{1, 7},
+		},
+		{
+			feed:        []string{"a", "2", "b"},
+			wantOffsets: []uint64{1001},
+			wantValues:  []int64{2},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(strings.Join(test.feed, ","), func(t *testing.T) {
+			var r intRecord
+
+			proxy := seeq.UnmarshalProxy[big.Int]{
+				Aggregate:     &r,
+				UnmarshalInto: unmarshalInto,
+			}
+
+			var batch []stream.Entry
+			for _, s := range test.feed {
+				batch = append(batch, stream.Entry{
+					MediaType: "text/plain",
+					Payload:   []byte(s),
+				})
+			}
+
+			err := proxy.AddNext(batch, 1000)
+			if err != nil {
+				t.Error("AddNext got error:", err)
+			}
+			if !slices.Equal(r.offsets, test.wantOffsets) {
+				t.Errorf("got offsets %d, want %d", r.offsets, test.wantOffsets)
+			}
+			if !slices.Equal(r.values, test.wantValues) {
+				t.Errorf("got values %d, want %d", r.values, test.wantValues)
+			}
+		})
+	}
 }
